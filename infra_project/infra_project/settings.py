2746d9095bd81bed61fff461c38d04a35f086da5
ckocmoca <39809358+ckocmoca@users.noreply.github.com>
2022-08-29 22:20:31 +0300

    Add ip in settings

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    ['158.160.3.131', '127.0.0.1', 'localhost'] 
+    '158.160.3.131',
+    '127.0.0.1',
+    'localhost'
 ]
 
 
